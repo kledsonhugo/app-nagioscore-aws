478f97db088e424dfe4adb5ef0d0fad524558e61
Kledson Basso <kledsonhugo@gmail.com>
2022-10-07 21:53:39 -0300

    Changing Required Version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,12 @@
 # PROVIDER
 terraform {
 
+required_version = "~> 1.3.1"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.34"
     }
   }
 
